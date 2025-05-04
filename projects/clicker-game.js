6b0399225836af8f0513c9560d8a6adb112f5ceb
JoeydaGOAT-coder <joey@kilians.us>
2025-05-04 11:21:56 -0500

    Update clicker-game.js

--- projects/clicker-game.js
+++ projects/clicker-game.js
@@ -1,27 +1,39 @@
 window.addEventListener('load', () => { 
   const button = document.getElementById("add");
   const output = document.getElementById("output");
-
+  const button1 = document.getElementById("add1");
+  const output1 = document.getElementById("output1");
+  
   let money = 0;
   let moneyPerClick = 1;
+  let numOfCoinUpgrades = 0;
+  let costOfCoinUpgrade = 10;
+  
   const coinClicked = () => {
     money += moneyPerClick
     return money;
   };
+  const coinUpgradeClicked = () => {
+    if (money >= costOfCoinUpgrade) {
+      money -= costOfCoinUpgrade;
+    };
+    return money;
+  };
+  const coinUpgradeClicked1 = () => {
+    if (money >= costOfCoinUpgrade) {
+      costOfCoinUpgrade *= 1.15;
+      costOfCoinUpgrade = Math.round(costOfCoinUpgrade);
+      ++ numOfCoinUpgrades;
+      ++moneyPerClick;
+      return "cost: " + costOfCoinUpgrade + "number: " + numOfCoinUpgrades;
+    };
+  };
+  
   button.addEventListener('click', () => {
     output.textContent = "money: " + coinClicked();
   });
-});
-window.addEventListener('load', () => { 
-  const button = document.getElementById("add");
-  const output = document.getElementById("output");
-
-  let numOfCoinUpgrades = 0;
-  const coinClicked = () => {
-    money += moneyPerClick
-    return money;
-  };
-  button.addEventListener('click', () => {
-    output.textContent = "money: " + coinClicked();
+  button1.addEventListener('click', () => {
+    output.textContent = "money: " + coinUpgradeClicked();
+    output1.textContent = coinUpgradeClicked1();
   });
 });
