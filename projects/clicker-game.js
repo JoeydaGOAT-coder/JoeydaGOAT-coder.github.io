fb64fd81f2fe90fa97ab5c2bea225f00658fd04d
JoeydaGOAT-coder <joey@kilians.us>
2025-05-04 10:42:43 -0500

    Update clicker-game.js

--- projects/clicker-game.js
+++ projects/clicker-game.js
@@ -12,3 +12,16 @@ window.addEventListener('load', () => {
     output.textContent = "money: " + coinClicked();
   });
 });
+window.addEventListener('load', () => { 
+  const button = document.getElementById("add");
+  const output = document.getElementById("output");
+
+  let numOfCoinUpgrades = 0;
+  const coinClicked = () => {
+    money += moneyPerClick
+    return money;
+  };
+  button.addEventListener('click', () => {
+    output.textContent = "money: " + coinClicked();
+  });
+});
